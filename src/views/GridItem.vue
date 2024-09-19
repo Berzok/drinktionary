6ea2c8c7d6cc827f02679ca1cd015f5992ad55cd
u.friedrich <ugo.friedrich@viveris.fr>
2024-09-19 15:18:03 +0200

    👽️: Mise à jour par rapport à remote

--- src/views/GridItem.vue
+++ src/views/GridItem.vue
@@ -35,7 +35,7 @@ const openModal = async () => {
   <ion-card class="item-link ion-activatable" @click="openModal">
     <ion-ripple-effect class="custom-ripple"/>
 
-    <img alt="Silhouette of mountains" class="drink-icon" :src="apiUrl + '/images/' + drink.icon "/>
+    <img alt="Silhouette of mountains" class="drink-icon" :src="apiUrl + '/uploads/images/' + drink.icon "/>
     <ion-card-header>
       <ion-card-title class="ion-text-center drink-name">{{ drink.name }}</ion-card-title>
     </ion-card-header>
